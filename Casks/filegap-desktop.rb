ee10545fa3ced70bf59cb435f13ceddc9e232773
Stefano <ste.valente@gmail.com>
2026-03-30 17:26:23 +0200

    fix(cask): interpolate version in desktop URL

--- Casks/filegap-desktop.rb
+++ Casks/filegap-desktop.rb
@@ -2,7 +2,7 @@ cask "filegap-desktop" do
   version "0.1.0"
   sha256 "f95a31738bb50f924c10b0b4b926867b689cb6546a5502d1fad793928abf67ce"
 
-  url "https://github.com/filegap/filegap/releases/download/desktop-v0.1.0/Filegap%20Desktop_0.1.0_aarch64.dmg"
+  url "https://github.com/filegap/filegap/releases/download/desktop-v#{version}/Filegap%20Desktop_#{version}_aarch64.dmg"
   name "Filegap Desktop"
   desc "Privacy-first local PDF desktop app"
   homepage "https://github.com/filegap/filegap"
