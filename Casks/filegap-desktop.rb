9996a60f38a6751cf5e3e6e20abc0848a3e30ba2
Stefano <ste.valente@gmail.com>
2026-03-30 17:19:33 +0200

    fix(cask): remove trailing blank line in filegap-desktop

--- Casks/filegap-desktop.rb
+++ Casks/filegap-desktop.rb
@@ -1,8 +1,8 @@
 cask "filegap-desktop" do
-  version "0.1.1"
-  sha256 "<SHA256_DMG>"
+  version "0.1.0"
+  sha256 "f95a31738bb50f924c10b0b4b926867b689cb6546a5502d1fad793928abf67ce"
 
-  url "https://github.com/filegap/filegap/releases/download/desktop-v0.1.1/Filegap%20Desktop_0.1.1_aarch64.dmg"
+  url "https://github.com/filegap/filegap/releases/download/desktop-v0.1.0/Filegap%20Desktop_0.1.0_aarch64.dmg"
   name "Filegap Desktop"
   desc "Privacy-first local PDF desktop app"
   homepage "https://github.com/filegap/filegap"
@@ -12,4 +12,3 @@ cask "filegap-desktop" do
 
   app "Filegap Desktop.app"
 end
-
