c280ff2fe65a5b5f11cdebbd80a1984689c26fae
Stefano <ste.valente@gmail.com>
2026-03-31 11:00:03 +0200

    fix(cask): point desktop URL to published v0.1.1 asset

--- Casks/filegap-desktop.rb
+++ Casks/filegap-desktop.rb
@@ -2,7 +2,7 @@ cask "filegap-desktop" do
   version "0.1.1"
   sha256 "f31be6efd0d5caff791ec8967f39b5ba174723e09fa9ed8787f8c446691ca311"
 
-  url "https://github.com/filegap/filegap/releases/download/desktop-v#{version}/Filegap%20Desktop_#{version}_aarch64.dmg"
+  url "https://github.com/filegap/filegap/releases/download/desktop-v#{version}/Filegap.Desktop_0.1.0_aarch64.dmg"
   name "Filegap Desktop"
   desc "Privacy-first local PDF desktop app"
   homepage "https://github.com/filegap/filegap"
