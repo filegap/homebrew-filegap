e8c1b75a3391c15951502a9c339beb97de5fc9ec
Stefano <ste.valente@gmail.com>
2026-04-07 17:21:04 +0200

    fix(cask): correct desktop v0.1.3 dmg filename

--- Casks/filegap-desktop.rb
+++ Casks/filegap-desktop.rb
@@ -2,7 +2,7 @@ cask "filegap-desktop" do
   version "0.1.3"
   sha256 "f431fcc6e2266dcb57d419352df7e0bd8dcba9621b86f47d8e6c0fa08ae95add"
 
-  url "https://github.com/filegap/filegap/releases/download/desktop-v#{version}/Filegap%20Desktop_0.1.3_aarch64.dmg"
+  url "https://github.com/filegap/filegap/releases/download/desktop-v#{version}/Filegap.Desktop_0.1.3_aarch64.dmg"
   name "Filegap Desktop"
   desc "Privacy-first local PDF desktop app"
   homepage "https://github.com/filegap/filegap"
